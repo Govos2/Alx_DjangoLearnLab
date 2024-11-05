cafe3fb7525abe11c58f7b2d776346d57b2f20dc
Achraf-221 <154558874+Govos2@users.noreply.github.com>
2024-11-05 20:07:58 +0100

    Create models.py, query_samples.py

---
+++ django-models/models.py, query_samples.py
@@ -0,0 +1,23 @@
+cp -r Introduction_to_Django django-models
+cd django-models
+python manage.py startapp relationship_app
+from django.db import models
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
